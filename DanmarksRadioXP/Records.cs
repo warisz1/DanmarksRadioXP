e396735b2d5de7e59d62f01e3453280cde5b0203
warisz1 <warisah2665@gmail.com>
2024-11-13 10:05:38 +0100

    Record class done

--- DanmarksRadioXP/Records.cs
+++ DanmarksRadioXP/Records.cs
@@ -28,5 +28,47 @@ namespace DanmarksRadioXP
             return $"{{{nameof(Id)}={Id.ToString()}, {nameof(Title)}={Title}, {nameof(Artist)}={Artist}, {nameof(Duration)}={Duration.ToString()}, {nameof(PublicationYear)}={PublicationYear.ToString()}}}";
         }
 
+        public void ValidateTitle()
+        {
+            if (Title == null) 
+            {
+                throw new ArgumentNullException("title is null");
+            }
+            if (Title.Length < 2)
+            {
+                throw new ArgumentOutOfRangeException("Title must be atleast 3 charchters");
+            }
+        }
+
+        public void ValidateArtist() 
+        {
+            if (Artist == null)
+            {
+                throw new ArgumentNullException("Artist is null");
+            }
+            if (Artist.Length < 2)
+            {
+                throw new ArgumentOutOfRangeException("Artist must be atleast 3 charchters");
+            }
+        }
+
+        public void ValidateDuration()
+        {
+            if (Duration < 1)
+            {
+                throw new ArgumentOutOfRangeException("Duration must be postive");
+            }
+        }
+
+        public void ValidatePublicationYear()
+        {
+            if (PublicationYear > 2024)
+            {
+                throw new ArgumentOutOfRangeException("Title cant be later 2024"); 
+            }
+        }
+
+
+
     }
 }
